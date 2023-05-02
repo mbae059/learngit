6b209bc5d0ad7a6fe81af72dbc86eb7217695b5f
Minjun Bae <mbae059@daum.net>
2023-05-03 08:18:36 +0900

    feat : Python env test
    Create do_fizzbuzz and test.

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+def do_fizzbuz():
+    """
+    fizzbuzz : print fizz buzz fizzbuzz
+    3 : fizz
+    5: buzz
+    15 : fizzbuzz
+    etc : num
+    """
+    print('hello')
+
+if __name__ =='__main__':
+    do_fizzbuzz()
